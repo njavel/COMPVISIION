e89359b421603b128c0a166f34eaffd20d37df9c
njavel <70972990+njavel@users.noreply.github.com>
2023-05-10 08:50:49 -0500

    Update sketcher.js

--- sketcher.js
+++ sketcher.js
@@ -34,75 +34,12 @@ let sketch = function (p) {
     p.createCanvas(cam_w, cam_h, p.WEBGL);
     p.setAttributes({ alpha: true });
     p.textFont(font);
+    p.textAlign("left");
   };
 
   p.draw = function () {
     p.clear(0);
     p.translate(-p.width / 2, -p.height / 2);
-
-    const cam_w = 640;
-const cam_h = 360;
-let capture;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
-  capture.size(cam_w, cam_h);
-  capture.hide();
-}
-
-function draw() {
-  clear();
-
-  capture.loadPixels();
-//dont proceed until there is camera feed
-  if (capture.pixels.length > 0) {
-    push();
-    scale(width/cam_w, width/cam_w);
-    mirror();
-    pop();
-  }
-}
-
-// ADD YOUR OWN VERSION OF THE MIRROR DOWN BELOW
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-  
-}
-  function mirror() {
-  
-  p.background(0,0,0);
-  const stepSize = 40;
-  
-  for (let y = 0; y < cam_h; y += stepSize) {
-    for (let x = 0; x < cam_w; x += stepSize) {
-      const index = ((cam_w - x) + y * cam_w) * 4;
-
-      const r = capture.pixels[index];
-      const g = capture.pixels[index + 1];
-      const b = capture.pixels[index + 2];
-      const brightness = (r + g + b) / 3;
-
-      // USE THE x, y, r, g, or b values to draw things to the screen
-      
-      
-      const size = map(brightness, 0, 255, stepSize/4, stepSize);
-      //noStroke();
-      stroke(r, g, b, 255);    
-      strokeWeight(10);
-      fill(r*2, brightness, 255);
-      
-      //ellipse(x, y, size, size)
-     rect(x, y, size*3, size*2);
-      textSize(brightness/2);
-      fill(180,brightness,200);
-      text("nikita", x, y);
-    blendMode(HARD_LIGHT);
-     // blendMode(REPLACE);
-
-    }
-  }
-}
 
     if (detections != undefined) {
       // console.log(detections);
@@ -110,7 +47,6 @@ function windowResized(){
       // check right hand
       // check left hand
       // check poses
-
 
       if (detections.poseLandmarks != undefined) {
         p.drawPose(detections.poseLandmarks);
@@ -129,7 +65,7 @@ function windowResized(){
     // p.pop();
 
     // add points to this array for each poseKeypoint you want to draw something on.
-    const myPosePoints = [0,3,6,7,8,9,11,12,13,14,23,25,26,27,28];
+    const myPosePoints = [0,3,6,7,8,9,11,12,13,14,15,16,23,25,26,27,28];
 
     for (let i = 0; i < myPosePoints.length; i++) {
       const curPoint = myPosePoints[i];
@@ -144,77 +80,127 @@ function windowResized(){
 
       switch (curPoint) {
 
-        case 0:
+        case 0: // crown chak, center skull
           p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
+          p.fill(111, 0, 255);
           p.textSize(12);
-          p.text("center skull",0,0);
+          p.text("gintang buko",0,-16);
           p.image(img, 0, 0, 30, 30);
           //p.textAlign(CENTER);
-          p.text("The heart holds significant spiritual symbolism in Arnis\nit represents one's innermost essence, integrity, and dedication to the practice\n Arnis practitioners strive to cultivate virtues such as compassion, respect, and love in their hearts\n",80,150);
+          p.text("Ulo•\nhouse of the soul\nintelligence•wisdom•spiritual power\nconnection to divine & ancestral spirits.\n",0,-100);
 
         break;
 
         case 3: //right eye
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("right eye",0,0);
-          p.fill(255,0,255);
-          p.textSize(10);
-          p.image(righteye, 0, 0, 15, 15);
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,8,8);
+          p.fill(115, 147, 255);
+          p.text("kanang mata",0,0,);
+          p.fill(172, 130, 255);
+          p.textSize(8);
+          p.text("Mata (Eyes)\n ability to see beyond physical\nworld symbolize awareness, intuition\n spiritual insight, perceive spirits\n &supernatural entities.",-190,0);
+          p.image(righteye, 0, 0, 18, 18);
         break;
 
-        case 6:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("left eye",0,0);
-          p.image(lefteye,0,0,15,15);
+        case 6: //left eye
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,8,8);
+          p.fill(115, 147, 255);
+          p.text("kaliwang mata",0,0);
+          p.fill(203, 186, 255);
+          p.textSize(5);
+          p.text("•use of eye amulets mata\nas protective charms against\n evil spirits & negative energy\n pagtatawas, divination using the lines and marks\n around the eyes to diagnose\n illnesses or spiritual afflictions\nritual of panawagan, where the babaylan (shaman) communicate\n with spirits & ancestors\nusing  eyes to convey or receive visions.",90,0);
+          p.image(lefteye,0,0,18,18);
         break;
 
-        case 7:
+        case 7: //right temple
+          p.fill(0, 255, 0);
           p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("right temple",0,0);
+          p.fill(255, 0, 0);
+          p.text("kanang templo",0,0);
           p.image(righttemple,0,0,20,20);
 
         break;
 
-        case 8:
+        case 8: //left temple
+          p.fill(0, 255, 0);
           p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("left temple",0,0);
+          p.fill(255, 0, 0);
+          p.text("kaliwang templo",0,0);
           p.image(lefttemple,0,0,20,20);
         break;
 
+        
+        case 9: // chest
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,10,10);
+          p.textSize(8);
+          p.ellipse(30,100,15,15);
+          p.fill(18, 166, 141);
+          p.image(lefttemple,0,0,20,20);
+          p.text("Dughan(Chest)\ncenter of emotions\nseat of innermost being\ndwelling place of the spirit\n",30,100);  
+        break;
+
         case 11:
+          p.fill(0, 255, 0);
           p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("right shoulder",0,0);
+          p.fill(245, 160, 115);
+          p.text("kanang balikat",0,0);
           p.image(rightshoulder,0,0,35,35);
-          p.text("Dinhi sa da\npit nato\nMga lumad nga magahapon",-80,-130);
+          p.text("Dinhi sa da\npit nato\nMga lumad nga magahapon",-100,-150);
           p.image(lullaby,-50,-100,40,40);
         break;
 
         case 12:
+          p.fill(0, 255, 0);
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
-          p.text("left shoulder",0,0);
+          p.text("kaliwang balikat",0,0);
           p.image(leftshoulder,0,0,35,35);
         break;
 
-        case 25:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("right knee",0,0);
+        case 15: //right hand
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,7,7);
+          p.fill(0, 255, 0)
+          p.text("kanang kamay",0,-10);
+          p.textSize(8);
+          p.image(img,0,0,35,35);
+          p.text("Kamao(Hands)\ninstruments of power & action\nsymbolize creativity &skill\n portal to connect w/spiritual realm",0,0);
+        break;
+
+        case 16: //left hand
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,7,7);
+          p.fill(255, 0, 255);
+          p.text("kaliwa kamay",0,-10);
+          p.textSize(8);
+          p.image(righteye,0,0,35,35);
+          p.text("Pagkamao(hand movements)\n hand gestures/movements in dance\n invoke spirits, conveys blessings",0,0);
+          
+        break;
+
+        case 25: //feet
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,9,9);
+          p.fill(255, 0, 13);
+          p.text("kanang paa",0,0);
           p.image(rightknee,0,0,20,20);
+          p.textSize(6);
+          p.text("Pagsuay\n babaylan (shaman) use their feet to massage\n & manipulate body ofthe patient\nto relieve pain\nremove blockages\n restore energy balance.",0,35);
+
 
         break;
 
-        case 26:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("left knee",0,0);
+        case 26: //feet
+          p.fill(0, 255, 0);
+          p.ellipse(0,0,9,9);
+          p.fill(255, 0, 13);
+          p.text("kaliwang paa",0,0);
           p.image(rightknee,0,0,20,20);
+          p.textSize(6);
+          p.text("Pag-alay sa Lupa\noffering to the land\nfarmers make offerings to ground\nbefore planting crops\ntouch ground barefoot\nexpress gratitude & seek blessings for fruitful harvest.",0,35);
+
         break;
 
 
