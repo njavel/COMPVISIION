965f34666a87a721aff01760f3c99fa22fce8259
njavel <70972990+njavel@users.noreply.github.com>
2023-05-10 06:42:02 -0500

    jjj

--- sketcher.js
+++ sketcher.js
@@ -5,16 +5,28 @@
 let sketch = function (p) {
   let font;
   let img;
-  let centerskull;
+ // let centerskull;
   let righteye;
   let lefteye;
+  let righttemple;
+  let lefttemple;
+  let rightshoulder;
+  let leftshoulder;
+  let rightknee;
+  let lullaby;
 
   p.preload = function () {
     font = p.loadFont("Poppins-Medium.ttf");
-    img = p.loadImage('yea.png');
-    centerskull = p.loadImage('centerskull.png');
-    righteye = p.loadImage('righteye.png');
-    leteye = p.loadImage('lefteye.png');
+    img = p.loadImage('scooby.jpg');
+    //centerskull = p.loadImage('centerskull.jpg');
+    righteye = p.loadImage('riighteye.jpg');
+    lefteye = p.loadImage('lefteye.jpg');
+    righttemple = p.loadImage('righttemple.jpg');
+    lefttemple = p.loadImage('lefttemple.jpg');
+    rightshoulder = p.loadImage('rightshoulder.jpg');
+    leftshoulder = p.loadImage('leftshoulder.jpg');
+    rightknee = p.loadImage('rightknee.jpg');
+    lullaby = p.loadImage('lullaby.jpg');
 
   };
 
@@ -22,12 +34,75 @@ let sketch = function (p) {
     p.createCanvas(cam_w, cam_h, p.WEBGL);
     p.setAttributes({ alpha: true });
     p.textFont(font);
-    p.textSize(16);
   };
 
   p.draw = function () {
     p.clear(0);
     p.translate(-p.width / 2, -p.height / 2);
+
+    const cam_w = 640;
+const cam_h = 360;
+let capture;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(cam_w, cam_h);
+  capture.hide();
+}
+
+function draw() {
+  clear();
+
+  capture.loadPixels();
+//dont proceed until there is camera feed
+  if (capture.pixels.length > 0) {
+    push();
+    scale(width/cam_w, width/cam_w);
+    mirror();
+    pop();
+  }
+}
+
+// ADD YOUR OWN VERSION OF THE MIRROR DOWN BELOW
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+  
+}
+  function mirror() {
+  
+  p.background(0,0,0);
+  const stepSize = 40;
+  
+  for (let y = 0; y < cam_h; y += stepSize) {
+    for (let x = 0; x < cam_w; x += stepSize) {
+      const index = ((cam_w - x) + y * cam_w) * 4;
+
+      const r = capture.pixels[index];
+      const g = capture.pixels[index + 1];
+      const b = capture.pixels[index + 2];
+      const brightness = (r + g + b) / 3;
+
+      // USE THE x, y, r, g, or b values to draw things to the screen
+      
+      
+      const size = map(brightness, 0, 255, stepSize/4, stepSize);
+      //noStroke();
+      stroke(r, g, b, 255);    
+      strokeWeight(10);
+      fill(r*2, brightness, 255);
+      
+      //ellipse(x, y, size, size)
+     rect(x, y, size*3, size*2);
+      textSize(brightness/2);
+      fill(180,brightness,200);
+      text("nikita", x, y);
+    blendMode(HARD_LIGHT);
+     // blendMode(REPLACE);
+
+    }
+  }
+}
 
     if (detections != undefined) {
       // console.log(detections);
@@ -36,13 +111,6 @@ let sketch = function (p) {
       // check left hand
       // check poses
 
-      if (detections.leftHandLandmarks != undefined) {
-        p.drawHand(detections.leftHandLandmarks);
-      }
-
-      if (detections.rightHandLandmarks != undefined) {
-        p.drawHand(detections.rightHandLandmarks);
-      }
 
       if (detections.poseLandmarks != undefined) {
         p.drawPose(detections.poseLandmarks);
@@ -68,9 +136,9 @@ let sketch = function (p) {
       const poseFeature = pose[curPoint];
       p.push();
       p.translate(p.width - poseFeature.x * p.width, poseFeature.y * p.height, 0);
-      p.scale(5);
+      p.scale(2);
       p.normalMaterial();
-      p.textSize(5);
+     // p.textSize(1);
 
       // a more efficient if/else thingy (https://www.w3schools.com/js/js_switch.asp)
 
@@ -79,63 +147,74 @@ let sketch = function (p) {
         case 0:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
+          p.textSize(12);
           p.text("center skull",0,0);
-          p.scale(.02);
-          p.image(centerskull,0,0);
+          p.image(img, 0, 0, 30, 30);
+          //p.textAlign(CENTER);
+          p.text("The heart holds significant spiritual symbolism in Arnis\nit represents one's innermost essence, integrity, and dedication to the practice\n Arnis practitioners strive to cultivate virtues such as compassion, respect, and love in their hearts\n",80,150);
+
         break;
 
-        case 3:
+        case 3: //right eye
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("right eye",0,0);
-          p.scale(.02);
-          p.image(righteye,0,0);
+          p.fill(255,0,255);
+          p.textSize(10);
+          p.image(righteye, 0, 0, 15, 15);
         break;
 
         case 6:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("left eye",0,0);
-          p.scale(.02);
-          p.image(lefteye,0,0);
+          p.image(lefteye,0,0,15,15);
         break;
 
         case 7:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("right temple",0,0);
+          p.image(righttemple,0,0,20,20);
+
         break;
 
         case 8:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("left temple",0,0);
+          p.image(lefttemple,0,0,20,20);
         break;
 
         case 11:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
-          p.text("right shoulder",-20,0);
-          p.scale(.05);
-          //p.image(img,0,0);
+          p.text("right shoulder",0,0);
+          p.image(rightshoulder,0,0,35,35);
+          p.text("Dinhi sa da\npit nato\nMga lumad nga magahapon",-80,-130);
+          p.image(lullaby,-50,-100,40,40);
         break;
 
         case 12:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("left shoulder",0,0);
+          p.image(leftshoulder,0,0,35,35);
         break;
 
         case 25:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("right knee",0,0);
+          p.image(rightknee,0,0,20,20);
+
         break;
 
         case 26:
           p.ellipse(0,0,5,5);
           p.fill(255, 187, 0);
           p.text("left knee",0,0);
+          p.image(rightknee,0,0,20,20);
         break;
 
 
@@ -144,88 +223,8 @@ let sketch = function (p) {
     }
   };
 
-  p.drawHand = function (hand) {
-    p.stroke(0);
-    p.strokeWeight(8);
-
-    const myHandPoints = [0, 4, 8, 12, 16, 20];
-
-    // const THUMB = hand[4];
-    // const INDEX_FINGER = hand[8];
-    // const MIDDLE_FINGER = hand[12];
-    // const RING_FINGER = hand[16];
-    // const PINKY_FINGER = hand[20];
-    // const WRISTY = hand[0];
-
-    for (let i = 0; i < myHandPoints.length; i++) {
-      const curPoint = myHandPoints[i];
-      const handFeature = hand[curPoint];
-      p.fill(255, 0, 255);
-      p.push();
-      p.translate(p.width - handFeature.x * p.width, handFeature.y * p.height, 0);
-      p.scale(5);
-      p.textSize(5);
-      //p.rotateY(180);
-
-      switch (curPoint) {
-        case 0:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("hibitch",0,0);
-        break;
-
-        case 4:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("hibitch",0,0);
-        break;
-
-        case 8:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("hibitch",0,0);
-        break;
-
-        case 12:
-          p.ellipse(0,0,5,5);
-          p.fill(255, 187, 0);
-          p.text("hibitch",0,0);
-        break;
-
-      p.pop();
-
-      }
-
-  
-    };
-
-    for (let i = 0; i < hand.length; i++) {
-      const x = p.width - hand[i].x * p.width;
-      const y = hand[i].y * p.height;
-      const z = hand[i].z;
-
-      p.strokeWeight(2);
-
-   // p.text(z.toFixed(3), x, y, z);
-      p.fill(255, 187, 0);
-      p.text("hey bitch", x, y, z);
-      p.textSize(10);
-
-      // 1calculate how far the hand is from the camera by calculating the distance between keypoints 9 and 13 (base of middle and ring finger)
-      if (i == 9 || i == 13) {
-        const palm1 = hand[9];
-        const palm2 = hand[13];
-
-        const palm1Pos = p.createVector(palm1.x, palm1.y);
-        const palm2Pos = p.createVector(palm2.x, palm2.y);
-
-        const distance = palm1Pos.dist(palm2Pos);
-
-        // console.log(distance);
-      }
-    }
   };
-};
+
 //p.background(255,0,255);
 let myp5 = new p5(sketch);
 
